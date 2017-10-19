f68f15c7396cad01edca2b69d3619e4317e63593
alexbcberio <alexbcberio@gmail.com>
2017-10-19 10:53:34 +0200

    Added smoothScroll to home and set header 100vh

--- public/assets/js/smoothScroll.js
+++ public/assets/js/smoothScroll.js
@@ -4,7 +4,17 @@ document.addEventListener("DOMContentLoaded", function() {
         let link = document.links[i]
         let href = link.href.replace(window.location.origin + "/", "");
 
-        if (href.charAt(0) == "#") {
+        // scroll top on /
+        if (window.location.pathname == "/" && link.href.replace(window.location.origin, "") == "/") {
+            link.addEventListener("click", function(e) {
+                e.preventDefault();
+
+                document.querySelector("header").scrollIntoView({behavior: "smooth", inline: "start"});
+                window.history.pushState(null, document.title, "");
+                setActive(this);
+            }, false);
+
+        }  else if (href.charAt(0) == "#") {
             link.addEventListener("click", scroll, false);
         }
     }
@@ -14,8 +24,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
         let id = this.href.replace(window.location.origin + "/#", "");
 
+        setActive(this);
         document.getElementById(id).scrollIntoView({behavior: "smooth", inline: "start"});
         window.history.pushState(null, document.title, "#" + id);
+    }
+
+    function setActive(element) {
+        let currentActive = document.getElementById("mainNavbar").querySelector(".active");
+        currentActive.className = currentActive.className.replace("active", "");
+
+        element.parentNode.className += " active";
+        console.log(element);
     }
 
 }, false);
