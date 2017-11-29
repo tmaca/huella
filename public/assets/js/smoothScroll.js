b04464a6043478761789451359d7fef06a78148b
alexbcberio <alexbcberio@gmail.com>
2017-11-29 14:00:10 +0100

    Bugfig: Scroll with javascript was scrolling 50px more

--- public/assets/js/smoothScroll.js
+++ public/assets/js/smoothScroll.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     // Add scrollspy to <body>
-    $('body').scrollspy({target: ".navbar", offset: 50});
+    $('body').scrollspy({target: ".navbar", offset: 51});
 
     // Add smooth scrolling on all links inside the navbar
     $("#mainNavbar a").on('click', function(event) {
@@ -14,12 +14,11 @@ $(document).ready(function(){
 
             // Using jQuery's animate() method to add smooth page scroll
             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 500, function(){
-
+            $('html').animate({
+                scrollTop: $(hash).offset().top - 50
+            }, 500, function() {
                 // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
+                window.history.pushState(null, hash, hash);
             });
         }  // End if
     });
