90ba460bffa3b7f96422f968c8d7dd0b3e7ece0f
alexbcberio <alexbcberio@gmail.com>
2017-12-01 10:21:31 +0100

    Merge branch 'addLandingContent'

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,31 +4,141 @@
 
 @section("content")
 
-    <div class="container-fluid" id="quienesSomos">
+    <section class="container-fluid" id="queEs" style="background: red;">
         <div class="container">
-            <div class="bloque">
-            <h1>Quienes somos</h1>
+            <h1>¿Qué es la huella de carbono?</h1>
+            <hr>
+            <div class="row">
+                <img src="{{ url("assets/img/huella.png") }}" class="hidden-xs col-sm-offset-0 col-sm-4 col-md-3 col-lg-2" alt="Huella de pie verde creado con bombillas, simbolo de reciclaje y otras imagenes que fomentan el ecologismo.">
+                <p class="col-xs-12 col-sm-8 col-md-9 col-lg-10 text-justify">
+                    El cambio climático, provocado por la emisión de Gases de Efecto Invernadero y en especial del CO<sup>2</sup>, es uno de los mayores probleas de nuestro tiempo y existen evidencias considerables de que la mayor parte del calentamiento global ha sido causado por las actividades humanas. Hoy día, casi todas las actividades que realizamos y bienes que poseemos y utilizamos implican <strong>consumir energía</strong>, lo que significa <strong>contribuir a las emisiones a la atmósfera</strong>. La <strong>Huella de Carbono</strong> se conoce como la totalidad de gases de efecto invernadero, también conocidos como GEI, emitidos por efecto directo (por el centro/organización) o indirecto (emitidos por terceros).
+                </p>
 
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in velit eget metus lobortis semper id vel ante. Mauris tempor sit amet felis ut aliquet. Pellentesque viverra aliquam volutpat. Nunc ligula nunc, lacinia ut convallis vitae, dapibus at diam. Nulla eu leo tempus nibh rutrum convallis in sit amet ex. Pellentesque tristique fermentum massa, eu fringilla arcu varius placerat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla ut erat at felis consectetur tincidunt. Vestibulum metus mi, finibus sed massa at, viverra commodo lorem. Vivamus sed risus pharetra, egestas mi a, posuere odio. Mauris quis velit et nisi interdum feugiat. Donec sagittis, justo at tempor luctus, lorem metus sollicitudin metus, eget cursus ex lacus quis diam. Vestibulum iaculis dolor eget leo pretium interdum.
-            </p>
-        </div><!--bloque-->
+                <img src="{{ url("assets/img/huella.png") }}" class="visible-xs col-xs-offset-4 col-xs-4" alt="Huella de pie verde creado con bombillas, simbolo de reciclaje y otras imagenes que fomentan el ecologismo.">
+
+                <p class="col-xs-12 col-sm-8 col-md-9 col-lg-10 text-justify">
+                    Bajo este prisma, la Huella de Carbono, representa una medida para la contribución de las organizaciones a ser entidades socialmente responsables y un elemento más de concienciación para la asunción entre los ciudadanos de prácticas más sostenibles. Con esta iniciativa se pretende cuantificar la cantidad de emisiones de GEI, medidas en emisiones de CO2 equivalente, que son liberadas a la atmósfera debido a nuestras actividades cotidianas o a la comercialización de un producto. Este análisis abarca todas las actividades de su ciclo de vida (desde la adquisición de las materias primas hasta su gestión como residuo) permitiendo a los consumidores decidir qué alimentos comprar en base a la contaminación generada como resultado de los procesos por los que ha pasado.
+                </p>
+
+                <div class="clearfix"></div>
+            </div>
         </div>
-    </div>
+    </section>
 
-    <div class="container-fluid" id="servicios">
+    <section class="container-fluid" id="comoFunciona" style="background: orange;">
         <div class="container">
-            <div class="bloque">
-            <h1>Servicios</h1>
+            <h1>Como funciona</h1>
+            <hr>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in velit eget metus lobortis semper id vel ante. Mauris tempor sit amet felis ut aliquet. Pellentesque viverra aliquam volutpat. Nunc ligula nunc, lacinia ut convallis vitae, dapibus at diam. Nulla eu leo tempus nibh rutrum convallis in sit amet ex. Pellentesque tristique fermentum massa, eu fringilla arcu varius placerat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla ut erat at felis consectetur tincidunt. Vestibulum metus mi, finibus sed massa at, viverra commodo lorem. Vivamus sed risus pharetra, egestas mi a, posuere odio. Mauris quis velit et nisi interdum feugiat. Donec sagittis, justo at tempor luctus, lorem metus sollicitudin metus, eget cursus ex lacus quis diam. Vestibulum iaculis dolor eget leo pretium interdum.
+            <div class="row">
+
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading text-center">
+                            <i class="fa fa-user-plus fa-4x"></i>
+                        </div>
+                        <div class="panel-body text-center">
+                            Registrate y verifica tu e-mail
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading text-center">
+                            <i class="fa fa-check-square-o fa-4x"></i>
+                        </div>
+                        <div class="panel-body text-center">
+                            Completa los formularios
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xs-12 col-sm-offset-3 col-sm-6 col-md-offset-0 col-md-4">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading text-center">
+                            <i class="fa fa-bar-chart fa-4x"></i>
+                        </div>
+                        <div class="panel-body text-center">
+                            Ve estadísticas de tus emisiones e históricos
+                        </div>
+                    </div>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+
+            <p class="text-justify">
+                Para calcular la Huella de Carbono de tu centro debes seguir los siguientes pasos, es muy sencillo. Primero <a href="{{ route("register") }}">registras</a> el centro, una vez registrada la cuenta <a href="{{ route("login") }}">inicias sesión</a> y cumplimentas los formularios, para algunos de ellos necesitarás tener las facturas a mano. Una vez finalizado se realizará el cálculo de la emisión, estos datos serán almacenados para que puedar ir realizando comparaciones de la evolución, todos estos datos serán mostrados en gráficos.
             </p>
-            </div><!--bloque-->
-        </div>
-    </div>
+            <p class="text-justify">
+                A continuación te mostramos una simulación de la evolución de la evolución del consumo durante un año.
+            </p>
 
-    <div class="container-fluid" id="contacto">
+            <div id="example-chart">
+                <canvas width="300" height="100"></canvas>
+
+                <script>
+                var ctx = document.getElementById("example-chart").getElementsByTagName("canvas")[0].getContext('2d');
+                var myChart = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: [
+                            "Enero",
+                            "Febrero",
+                            "Marzo",
+                            "Abril",
+                            "Mayo",
+                            "Junio",
+                            "Julio",
+                            "Agosto",
+                            "Septiembre",
+                            "Octubre",
+                            "Noviembre",
+                            "Diciembre",
+                        ],
+                        datasets: [{
+                            data: [
+                                Math.random() *10,
+                                Math.random() *10,
+                                Math.random() *10,
+                                Math.random() *10,
+                                Math.random() *10,
+                                Math.random() *10,
+                                Math.random() *10,
+                                Math.random() *10,
+                                Math.random() *10,
+                                Math.random() *10,
+                                Math.random() *10,
+                                Math.random() *10,
+                            ],
+                            backgroundColor: "rgba(60, 184, 62, 0.3)",
+                            borderColor: "#3cb83e",
+                            borderWidth: 3,
+                        }]
+                    },
+                    options: {
+                        legend: {
+                            display: false,
+                            },
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true,
+                                }
+                            }]
+                        }
+                    }
+                });
+                </script>
+
+            </div>
+        </div>
+    </section>
+
+    <section class="container-fluid" id="contacto" style="background: red;">
         <div class="container">
+<<<<<<< HEAD
             <div class="bloque">
                 <div class="row">
                     <div class="col-md-12">
@@ -54,9 +164,34 @@
                             <input type="submit" value="Enviar mensaje " class="btn btn-success">
                         </form>
                     </div>
+=======
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Contacto</h1>
+                    <hr>
+                    <form action="{{ url('contact') }}" method="POST">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <label name="email">Email:</label>
+                            <input id="email" name="email" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <label name="subject">Asunto:</label>
+                            <input id="subject" name="subject" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <label name="message">Mensaje:</label>
+                            <textarea id="message" name="message" class="form-control">Escribe tu mensaje aqui...</textarea>
+                        </div>
+
+                        <input type="submit" value="Enviar mensaje " class="btn btn-success">
+                    </form>
+>>>>>>> addLandingContent
                 </div>
-            </div><!--bloque-->
+            </div>
         </div>
-    </div>
+    </section>
 
 @endsection
