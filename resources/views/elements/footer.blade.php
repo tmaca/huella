063fdf7a4a065d1d8e1ef461f56f42c8a5bdfda1
tmaca <tmaca312@gmail.com>
2017-12-01 12:35:49 +0100

    Footer changes.

--- resources/views/elements/footer.blade.php
+++ resources/views/elements/footer.blade.php
@@ -1,7 +1,10 @@
 
 <footer>
-
-    <p>Footer, work in progress</p>
-
+    <div id="imagenfooter">
+        <img src="assets/img/human-footprints.svg" width="80" height="80" />
+    </div>
+    <div id="textofooter">
+        <p>Huella de Carbono &copy; 2018</p>
+    </div>
 
 </footer>
