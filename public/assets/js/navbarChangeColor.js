a86f0e324b19ac38ad48fe713b1ec7932f880db4
jbustoro <jbustoro16dw@ikzubirimanteo.com>
2017-12-03 13:35:37 +0100

    commit

--- public/assets/js/navbarChangeColor.js
+++ public/assets/js/navbarChangeColor.js
@@ -3,9 +3,7 @@ $(document).ready(function() {
         if ($("#initial").attr('class').indexOf("active") !== -1){
             $("#mainNavbar").removeClass("navbar-green");
             $("#mainNavbar").addClass("navbar-transparent");
-            console.log("si");
         } else {
-            console.log("no");
             $("#mainNavbar").removeClass("navbar-transparent");
             $("#mainNavbar").addClass("navbar-green");
         }
