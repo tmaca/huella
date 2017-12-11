77becf54eebf4a4e2549c867f1e76940f824845d
alexbcberio <alexbcberio@gmail.com>
2017-12-11 09:13:59 +0100

    Implemented user edition

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 use App\Models\User;
 
@@ -36,7 +38,28 @@ class AdminController extends Controller
     }
 
     public function editUser(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|min:5|max:255',
+            'nif' => 'required|alpha_num|size:9',
+            'telephone' => 'required|integer|min:100000000|max:999999999',
+            'year' => 'required|integer|min:1000|max:2100',
+            'email' => 'required|string|email|max:255',
+            'verified' => 'required', Rule::in("0", "1"),
+        ]);
+
+        if ($validator->fails()) {
+            $request->session()->flash("editFailed", true);
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
         $user = User::where("id", $request->id)->firstOrFail();
-        return $request->all();
+        $user->name = $request->input("name");
+        $user->nif = $request->input("nif");
+        $user->telephone = $request->input("telephone");
+        $user->year = $request->input("year");
+        $user->email = $request->input("email");
+        $user->verified = $request->input("verified");
+        $user->save();
+        return redirect(route("admin.home"));
     }
 }
--- resources/views/forms/admin/editUser.blade.php
+++ resources/views/forms/admin/editUser.blade.php
@@ -1,60 +1,90 @@
 <form action="{{ route("admin.user.edit") }}" class="form-horizontal" method="post" novalidate>
     {{ csrf_field() }}
 
-    <input type="hidden" name="id" id="id">
+    <input type="hidden" name="id" id="id" value="{{ old("id") }}">
 
-    <div class="form-group{{ $errors->has("name") ? "has-error" : "" }}">
+    <div class="form-group{{ $errors->has("name") ? " has-error" : "" }}">
         <label for="name" class="col-sm-2 control-label">Nombre</label>
 
         <div class="col-sm-10">
-            <input type="text" id="name" class="form-control" autofocus>
+            <input type="text" name="name" id="name" class="form-control" autofocus value="{{ old("name") }}">
+            @if ($errors->has('name'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('name') }}</strong>
+                </span>
+            @endif
         </div>
     </div>
 
-    <div class="form-group{{ $errors->has("nif") ? "has-error" : "" }}">
+    <div class="form-group{{ $errors->has("nif") ? " has-error" : "" }}">
         <label for="nif" class="col-sm-2 control-label">NIF</label>
 
         <div class="col-sm-10">
-            <input type="text" name="nif" id="nif" class="form-control" autofocus>
+            <input type="text" name="nif" id="nif" class="form-control" value="{{ old("nif") }}">
+            @if ($errors->has('nif'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('nif') }}</strong>
+                </span>
+            @endif
         </div>
     </div>
 
-    <div class="form-group{{ $errors->has("telephone") ? "has-error" : "" }}">
+    <div class="form-group{{ $errors->has("telephone") ? " has-error" : "" }}">
         <label for="telephone" class="col-sm-2 control-label">Teléfono</label>
 
         <div class="col-sm-10">
-            <input type="text" name="telephone" id="telephone" class="form-control" autofocus>
+            <input type="number" name="telephone" id="telephone" class="form-control" value="{{ old("telephone") }}">
+            @if ($errors->has('telephone'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('telephone') }}</strong>
+                </span>
+            @endif
         </div>
     </div>
 
-    <div class="form-group{{ $errors->has("year") ? "has-error" : "" }}">
+    <div class="form-group{{ $errors->has("year") ? " has-error" : "" }}">
         <label for="year" class="col-sm-2 control-label">Año</label>
 
         <div class="col-sm-10">
-            <input type="text" name="year" id="year" class="form-control" autofocus>
+            <input type="text" name="year" id="year" class="form-control" value="{{ old("year") }}">
+            @if ($errors->has('year'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('year') }}</strong>
+                </span>
+            @endif
         </div>
     </div>
 
-    <div class="form-group{{ $errors->has("email") ? "has-error" : "" }}">
+    <div class="form-group{{ $errors->has("email") ? " has-error" : "" }}">
         <label for="email" class="col-sm-2 control-label">Email</label>
 
         <div class="col-sm-10">
-            <input type="text" name="email" id="email" class="form-control" autofocus>
+            <input type="text" name="email" id="email" class="form-control" value="{{ old("email") }}">
+            @if ($errors->has('email'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+            @endif
         </div>
     </div>
 
-    <div class="form-group{{ $errors->has("verified") ? "has-error" : "" }}">
+    <div class="form-group{{ $errors->has("verified") ? " has-error" : "" }}">
         <div class="col-sm-offset-2 col-sm-10">
             <div class="checkbox">
                 <label>
-                    <input type="radio" name="verified" value="0">
+                    <input type="radio" name="verified" value="0" @if(old("verified") == false){{ "checked" }}@endif>
                     No Verificada
                 </label>
                 <label>
-                    <input type="radio" name="verified" value="1">
+                    <input type="radio" name="verified" value="1" @if(old("verified") == true){{ "checked" }}@endif>
                     Verificada
                 </label>
             </div>
+            @if ($errors->has('verified'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('verified') }}</strong>
+                </span>
+            @endif
         </div>
     </div>
 
@@ -68,3 +98,9 @@
     </div>
 
 </form>
+
+@if(Request::session()->get("editFailed"))
+<script type="text/javascript">
+    $("#editUserModal").modal("show");
+</script>
+@endif
