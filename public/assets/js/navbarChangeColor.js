9816db07f7e0ee3530c29fd392088913aed4f56a
alexbcberio <alexbcberio@gmail.com>
2017-12-11 09:57:40 +0100

    Fixed navbar color update on landing

--- public/assets/js/navbarChangeColor.js
+++ public/assets/js/navbarChangeColor.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
-    $(window).on('scroll', function() {
-        if ($("#initial").attr('class').indexOf("active") !== -1){
-            $("#mainNavbar").removeClass("navbar-green");
-            $("#mainNavbar").addClass("navbar-transparent");
-        } else {
-            $("#mainNavbar").removeClass("navbar-transparent");
-            $("#mainNavbar").addClass("navbar-green");
-        }
-    });
+    $(window).on('scroll', updateNavbarColor);
+
+    updateNavbarColor();
 });
+
+function updateNavbarColor() {
+    if ($("#initial").attr('class').indexOf("active") !== -1){
+        $("#mainNavbar").removeClass("navbar-green");
+        $("#mainNavbar").addClass("navbar-transparent");
+    } else {
+        $("#mainNavbar").removeClass("navbar-transparent");
+        $("#mainNavbar").addClass("navbar-green");
+    }
+}
