1e4a3a80581598eb463355caf88d4d8ce5bd7db0
jbustoro <jbustoro16dw@ikzubirimanteo.com>
2017-12-12 11:36:50 +0100

    redirect to home at login, changed navbar colors

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -31,7 +31,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
