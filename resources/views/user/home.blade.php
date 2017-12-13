1c3cad4deee6a3756b3f3af8f8266b2354ce2701
tmaca <tmaca312@gmail.com>
2017-12-13 10:44:24 +0100

    formulario home user alcance 1

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -29,64 +29,7 @@
 
     <div id="formularioUser">
 
-        <form action="{{ ('#') }}" method="POST" class="form-inline">
-            {{ csrf_field() }}
-            <!--GASES-->
-
-                <label name="gasnatural" class="col-control-label">Gas natural (kwh)</label>
-                <input id="gasnatural" name="gasnatural" class="form-control">
-
-
-
-                <label name="gasnaturalNm" class="col-control-label">Gas natural (Nm3)</label>
-                <input id="gasnaturalNm" name="gasnaturalNm" class="form-control">
-
-
-            <div class="form-group">
-                <label name="refrigerantes" class="col-control-label">Refrigerantes</label>
-                <input id="refrigerantes" name="refrigerantes" class="form-control">
-            </div>
-
-            <div class="form-group">
-                <label name="recarga" class="col-control-label">Recarga gases refrigerantes</label>
-                <input id="recarga" name="recarga" class="form-control">
-            </div>
-                <!--GASES-->
-
-                <!--ELECTRICIDAD-->
-            <div class="form-group" >
-                <label name="electricidad" class="col-control-label">Electricidad KWh</label>
-                <input id="electricidad" name="electricidad" class="form-control">
-            </div>
-                <!--ELECTRICIDAD-->
-
-                <!--agua-->
-            <div class="form-group">
-                <label name="agua" class="col-control-label">Agua potable (m3)</label>
-                <input id="agua" name="agua" class="form-control">
-            </div>
-                <!--agua-->
-
-                <!--papel y carton-->
-            <div class="form-group">
-                <label name="papelcartonC" class="col-control-label">Papel y carton (consumo)</label>
-                <input id="papelcartonC" name="papelcartonC" class="form-control">
-            </div>
-
-            <div class="form-group">
-                <label name="papelcartonR" class="col-control-label">Papel y carton (residuos)</label>
-                <input id="papelcartonR" name="papelcartonR" class="form-control">
-            </div>
-                <!--papel y carton-->
-
-                <!--factor-->
-            <div class="form-group">
-                <label name="email" class="col-control-label">Factor kwh/Nm3</label>
-                <input id="email" name="email" class="form-control">
-            </div>
-                <!--factor-->
-            <input type="submit" value="Guardar" class="btn btn-success"/>
-        </form>
+        @include("forms.user.alcances")
 
     </div>
 </div><!--wrapper-->
