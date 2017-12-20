02438706d922a69b1636f9fbca99982376c6f7bf
alexbcberio <alexbcberio@gmail.com>
2017-12-20 10:07:33 +0100

    Showed messages after register and mail confirmation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,23 +5,6 @@
 @section("content")
     <div class="container">
 
-        @if(isset($postRegister) && $postRegister)
-        <script type="text/javascript">
-            document.addEventListener("DOMContentLoaded", function () {
-                swal("Activación de cuenta", "Se ha enviado un email de confirmación a la dirección indicada, {{ $emailAddress }}, una vez confirmado el email se activará la cuenta", "info");
-            });
-        </script>
-
-        <noscript>
-            <div class="alert alert-info">
-                <h4>Activación de cuenta</h4>
-                <p>
-                    Se ha enviado un email de confirmación a la dirección indicada, {{ $emailAddress }}, una vez confirmado el email se activará la cuenta
-                </p>
-            </div>
-        </noscript>
-        @endif
-
         @include("forms.auth.register")
     </div>
 
--- resources/views/elements/header.blade.php
+++ resources/views/elements/header.blade.php
@@ -7,6 +7,45 @@
     </div>
     <div id="superpuesto">
         <div class="vertical-aligned">
+
+            @if(Request::session()->get("postRegister"))
+            <script type="text/javascript">
+                document.addEventListener("DOMContentLoaded", function () {
+                    swal("Activación de cuenta", "Se ha enviado un email de confirmación a la dirección indicada, {{ Request::session()->get("emailAddress") }}, una vez confirmado el email se activará la cuenta", "info");
+                });
+            </script>
+
+            <noscript>
+                <div class="alert alert-info">
+                    <h4>Activación de cuenta</h4>
+                    <p>
+                        Se ha enviado un email de confirmación a la dirección indicada, {{ Request::session()->get("emailAddress") }}, una vez confirmado el email se activará la cuenta
+                    </p>
+                </div>
+            </noscript>
+            @endif
+
+            @if(Request::session()->get("accountActivated"))
+            <script type="text/javascript">
+                document.addEventListener("DOMContentLoaded", function () {
+                    swal("Cuenta activada", "Se ha realizado la activación de la cuenta, {{ Request::session()->get("emailAddress") }}", "success").then(function() {
+                        $("#loginModal #email").val("{{ Request::session()->get("emailAddress") }}");
+                        $("#loginModal").modal("show");
+                    });
+                });
+            </script>
+
+            <noscript>
+                <div class="alert alert-success">
+                    <h4>Cuenta activada</h4>
+                    <p>
+                        Se ha enviado un email de confirmación a la dirección indicada, {{ Request::session()->get("emailAddress") }}, una vez confirmado el email se activará la cuenta
+
+                    </p>
+                </div>
+            </noscript>
+            @endif
+
             <div>
                 <h2>
                     Descubre más sobre la Huella de Carbono
