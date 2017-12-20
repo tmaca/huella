1d3d5819dd24739006fa996f986e899aa231801c
jbustoro <jbustoro16dw@ikzubirimanteo.com>
2017-12-20 13:30:06 +0100

    buildings and alcances table migrations added

---
+++ database/migrations/2017_12_19_112307_create_alcances2_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlcances2Table extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alcances2', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('building_id')->unsigned();
+            $table->foreign('building_id')->references('id')->on('buildings')->onDelete("cascade")->onUpdate("cascade");
+            $table->string('year');
+            $table->string("electricidad_kwh");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alcances2');
+    }
+}
