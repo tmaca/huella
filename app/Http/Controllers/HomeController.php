040cf3cb10514e163c7cb98da60f92008b49fda0
alexbcberio <alexbcberio@gmail.com>
2018-01-15 08:58:37 +0100

    User home redirect to buildings list

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,7 +37,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('user.home');
+        return redirect(route("building"));
     }
 
     /**
