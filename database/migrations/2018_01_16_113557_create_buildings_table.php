bea51c99ab17ef664baed6f37be86d8a6eb91146
alexbcberio <alexbcberio@gmail.com>
2018-01-16 12:45:44 +0100

    Set cascade for delete and update on user field

--- database/migrations/2018_01_16_113557_create_buildings_table.php
+++ database/migrations/2018_01_16_113557_create_buildings_table.php
@@ -16,7 +16,7 @@ class CreateBuildingsTable extends Migration
         Schema::create('buildings', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate("cascade")->onDelete("cascade");
             $table->string('name');
             $table->text('description')->nullable();
             $table->integer("country_id")->unsigned();
