120507df0e0aa7ba5dce34c4f92f340fadea1798
alexbcberio <alexbcberio@gmail.com>
2018-01-16 22:44:04 +0100

    Added new fields to creation form

--- resources/views/forms/building/addBuilding.blade.php
+++ resources/views/forms/building/addBuilding.blade.php
@@ -10,15 +10,56 @@
     </div>
 
     <div class="form-group">
-        <select name="Description" class="form-control{{ $errors->has("Description") ? " is-invalid" : "" }}" placeholder="Descripción del edificio">
+        <textarea name="description" rows="3" class="form-control{{ $errors->has("desciption") ? " is-invalid" : "" }}" placeholder="Descripción del centro (opcional)">{{ old("description") }}</textarea>
+        @if ($errors->has('desciption'))
+            <div class="invalid-feedback">
+                <strong>{{ $errors->first('desciption') }}</strong>
+            </div>
+        @endif
+    </div>
+
+    <div class="form-group">
+        <select name="country_id" class="form-control{{ $errors->has("country_id") ? " is-invalid" : "" }}" placeholder="Descripción del edificio">
             <option disabled selected>--Selecciona un pais--</option>
             @foreach (App\Models\Country::all() as $country)
                 <option value="{{ $country->id }}">{{ $country->name }}</option>
             @endforeach
         </select>
-        @if ($errors->has('Description'))
+        @if ($errors->has('country_id'))
             <div class="invalid-feedback">
-                <strong>{{ $errors->first('Description') }}</strong>
+                <strong>{{ $errors->first('country_id') }}</strong>
+            </div>
+        @endif
+    </div>
+
+    <div class="form-group">
+        <select name="region_id" class="form-control{{ $errors->has("region_id") ? " is-invalid" : "" }}" placeholder="Descripción del edificio">
+            <option disabled selected>--Selecciona una provincia--</option>
+            @foreach (App\Models\Region::where("country_id", $country->id)->get() as $region)
+                <option value="{{ $region->id }}">{{ $region->name }}</option>
+            @endforeach
+        </select>
+        @if ($errors->has('region_id'))
+            <div class="invalid-feedback">
+                <strong>{{ $errors->first('region_id') }}</strong>
+            </div>
+        @endif
+    </div>
+
+    <div class="form-group">
+        <input type="number" name="postcode" class="form-control{{ $errors->has("postcode") ? " is-invalid" : "" }}" value="{{ old("postcode") }}" placeholder="Introduce código postal">
+        @if ($errors->has('postcode'))
+            <div class="invalid-feedback">
+                <strong>{{ $errors->first('postcode') }}</strong>
+            </div>
+        @endif
+    </div>
+
+    <div class="form-group">
+        <input type="text" name="address" class="form-control{{ $errors->has("address") ? " is-invalid" : "" }}" value="{{ old("address") }}" placeholder="Calle y número">
+        @if ($errors->has('address'))
+            <div class="invalid-feedback">
+                <strong>{{ $errors->first('address') }}</strong>
             </div>
         @endif
     </div>
