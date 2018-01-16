cca032a9307b45a1f7afdea035dbc18ceee9e4af
alexbcberio <alexbcberio@gmail.com>
2018-01-16 12:39:19 +0100

    Created country migration and model

---
+++ app/Models/Country.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    protected $table = 'countries';
+    protected $fillable = [
+        'name',
+    ];
+
+    public function provinces()
+    {
+        return $this->hasMany('App\Models\Region');
+    }
+}
