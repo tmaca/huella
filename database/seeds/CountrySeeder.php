45e9e4e34bfdd04c1d816d30793ba3e1da7bcf92
alexbcberio <alexbcberio@gmail.com>
2018-01-16 13:28:03 +0100

    Created country and region seeders

---
+++ database/seeds/CountrySeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Models\Country;
+
+class CountrySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $country = new Country;
+        $country->name = "España";
+        $country->save();
+    }
+}
---
+++ database/seeds/RegionSeeder.php
@@ -0,0 +1,77 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Models\Region;
+
+class RegionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $countryId = App\Models\Country::where("name", "España")->firstOrFail()->id;
+        $regions = [
+            "A Coruña",
+            "Álava",
+            "Albacete",
+            "Alicante",
+            "Almería",
+            "Asturias",
+            "Ávila",
+            "Badajoz",
+            "Islas Baleares",
+            "Barcelona",
+            "Burgos",
+            "Cáceres",
+            "Cádiz",
+            "Cantabria",
+            "Castellón",
+            "Ciudad Real",
+            "Córdoba",
+            "Cuenca",
+            "Girona",
+            "Granada",
+            "Guadalajara",
+            "Guipúzcoa",
+            "Huelva",
+            "Huesca",
+            "Jaén",
+            "La Rioja",
+            "Las Palmas",
+            "León",
+            "Lleida",
+            "Lugo",
+            "Madrid",
+            "Málaga",
+            "Murcia",
+            "Navarra",
+            "Orense",
+            "Palencia",
+            "Pontevedra",
+            "Salamanca",
+            "Segovia",
+            "Sevilla",
+            "Soria",
+            "Tarragona",
+            "Santa Cruz de Tenerife",
+            "Teruel",
+            "Toledo",
+            "Valencia",
+            "Valladolid",
+            "Vizcaya",
+            "Zamora",
+            "Zaragoza",
+        ];
+
+        foreach($regions as $regionName) {
+            $region = new Region;
+            $region->country_id = $countryId;
+            $region->name = $regionName;
+            $region->save();
+        }
+    }
+}
