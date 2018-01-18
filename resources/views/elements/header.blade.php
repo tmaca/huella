86a1fc733d32dd817eb448f1bfb9f3935dc9c4dd
jbustoro <jbustoro16dw@ikzubirimanteo.com>
2018-01-18 14:04:50 +0100

    changes in title animation, header text 100px, timeout between character 25, timeot when resolve completes 30000

--- resources/views/elements/header.blade.php
+++ resources/views/elements/header.blade.php
@@ -69,7 +69,7 @@
 
             <div class="titleContainer">
                 <h2 class="headerText text-uppercase" data-target-resolver>
-                    Huella De Carbono
+                    Huella de Carbono
                 </h2>
             </div>
                 <i class="fa fa-chevron-down fa-3x botonHeader whiteScroll" aria-hidden="true"></i>
@@ -80,73 +80,73 @@
 
 <script>
     const resolver = {
-    resolve: function resolve(options, callback) {
-    // The string to resolve
-    const resolveString = options.resolveString || options.element.getAttribute('data-target-resolver');
-    const combinedOptions = Object.assign({}, options, {resolveString: resolveString});
+        resolve: function resolve(options, callback) {
+            // The string to resolve
+            const resolveString = options.resolveString || options.element.getAttribute('data-target-resolver');
+            const combinedOptions = Object.assign({}, options, {resolveString: resolveString});
 
-    function getRandomInteger(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    };
+            function getRandomInteger(min, max) {
+              return Math.floor(Math.random() * (max - min + 1)) + min;
+            };
 
-    function randomCharacter(characters) {
-      return characters[getRandomInteger(0, characters.length - 1)];
-    };
+            function randomCharacter(characters) {
+              return characters[getRandomInteger(0, characters.length - 1)];
+            };
 
-    function doRandomiserEffect(options, callback) {
-      const characters = options.characters;
-      const timeout = options.timeout;
-      const element = options.element;
-      const partialString = options.partialString;
+            function doRandomiserEffect(options, callback) {
+              const characters = options.characters;
+              const timeout = options.timeout;
+              const element = options.element;
+              const partialString = options.partialString;
 
-      let iterations = options.iterations;
+              let iterations = options.iterations;
 
-      setTimeout(() => {
-        if (iterations >= 0) {
-          const nextOptions = Object.assign({}, options, {iterations: iterations - 1});
+              setTimeout(() => {
+                if (iterations >= 0) {
+                  const nextOptions = Object.assign({}, options, {iterations: iterations - 1});
 
-          // Ensures partialString without the random character as the final state.
-          if (iterations === 0) {
-            element.textContent = partialString;
-          } else {
-            // Replaces the last character of partialString with a random character
-            element.textContent = partialString.substring(0, partialString.length - 1) + randomCharacter(characters);
-          }
+                  // Ensures partialString without the random character as the final state.
+                  if (iterations === 0) {
+                    element.textContent = partialString;
+                  } else {
+                    // Replaces the last character of partialString with a random character
+                    element.textContent = partialString.substring(0, partialString.length - 1) + randomCharacter(characters);
+                  }
 
-          doRandomiserEffect(nextOptions, callback)
-        } else if (typeof callback === "function") {
-          callback();
+                  doRandomiserEffect(nextOptions, callback)
+                } else if (typeof callback === "function") {
+                  callback();
+                }
+              }, options.timeout);
+            };
+
+            function doResolverEffect(options, callback) {
+              const resolveString = options.resolveString;
+              const characters = options.characters;
+              const offset = options.offset;
+              const partialString = resolveString.substring(0, offset);
+              const combinedOptions = Object.assign({}, options, {partialString: partialString});
+
+              doRandomiserEffect(combinedOptions, () => {
+                const nextOptions = Object.assign({}, options, {offset: offset + 1});
+
+                if (offset <= resolveString.length) {
+                  doResolverEffect(nextOptions, callback);
+                } else if (typeof callback === "function") {
+                  callback();
+                }
+              });
+            };
+
+            doResolverEffect(combinedOptions, callback);
         }
-      }, options.timeout);
-    };
-
-    function doResolverEffect(options, callback) {
-      const resolveString = options.resolveString;
-      const characters = options.characters;
-      const offset = options.offset;
-      const partialString = resolveString.substring(0, offset);
-      const combinedOptions = Object.assign({}, options, {partialString: partialString});
-
-      doRandomiserEffect(combinedOptions, () => {
-        const nextOptions = Object.assign({}, options, {offset: offset + 1});
-
-        if (offset <= resolveString.length) {
-          doResolverEffect(nextOptions, callback);
-        } else if (typeof callback === "function") {
-          callback();
-        }
-      });
-    };
-
-    doResolverEffect(combinedOptions, callback);
-    }
     }
 
     /* Some GLaDOS quotes from Portal 2 chapter 9: The Part Where He Kills You
     * Source: http://theportalwiki.com/wiki/GLaDOS_voice_lines#Chapter_9:_The_Part_Where_He_Kills_You
     */
     const strings = [
-    'HUELLA DE CARBONO'
+    'Huella de Carbono'
     ];
 
     let counter = 0;
@@ -155,7 +155,7 @@
     // Initial position
     offset: 0,
     // Timeout between each random character
-    timeout: 5,
+    timeout: 25,
     // Number of random characters to show
     iterations: 10,
     // Random characters to pick from
@@ -168,16 +168,16 @@
 
     // Callback function when resolve completes
     function callback() {
-    setTimeout(() => {
-    counter ++;
+        setTimeout(() => {
+            counter ++;
 
-    if (counter >= strings.length) {
-      counter = 0;
-    }
+            if (counter >= strings.length) {
+              counter = 0;
+            }
 
-    let nextOptions = Object.assign({}, options, {resolveString: strings[counter]});
-    resolver.resolve(nextOptions, callback);
-    }, 1000);
+            let nextOptions = Object.assign({}, options, {resolveString: strings[counter]});
+            resolver.resolve(nextOptions, callback);
+        }, 30000);
     }
 
     resolver.resolve(options, callback);
