dc9a74cac10fdf8e002f5644f3bc38e573d742eb
alexbcberio <alexbcberio@gmail.com>
2018-01-19 09:11:35 +0100

    Implemented change of coordinates

--- public/assets/js/user/buildingManagement.js
+++ public/assets/js/user/buildingManagement.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     addRemoveEvent();
     addEditEvent();
+    addSetCoordinates();
 }, false);
 
 function addRemoveEvent() {
@@ -12,6 +13,12 @@ function addRemoveEvent() {
 function addEditEvent() {
     for (element of document.querySelectorAll("[data-action=\"edit\"]")) {
         element.addEventListener("click", editBuilding, false);
+    }
+}
+
+function addSetCoordinates() {
+    for (element of document.querySelectorAll(".coordinates > a")) {
+        element.addEventListener("click", setCoordinates, false);
     }
 }
 
@@ -63,6 +70,100 @@ function editBuilding() {
     document.getElementById("address").value = row.getElementsByClassName("address")[0].innerText;
 
     $("#editBuildingModal").modal("show");
+}
+
+let map,
+    marker,
+    building_id;
+
+function setCoordinates() {
+    let coordinates = this.parentNode;
+    let latitude = coordinates.getAttribute("data-latitude");
+    let longitude = coordinates.getAttribute("data-longitude");
+    building_id = coordinates.parentNode.getElementsByClassName("id")[0].innerText;
+
+    if (latitude && longitude) {
+        // center map on current coordinates with X zoom
+        showMap(latitude, longitude);
+    } else {
+        showMap();
+        // center map on country
+    }
+
+    $("#mapModal").modal("show");
+}
+function showMap(lat, lng) {
+    let zoom = 15;
+    if (!lat || !lng) {
+        lat = 40.393;
+        lng = -3.702;
+        zoom = 5;
+    }
+
+    if (!map) {
+        $('#mapModal').one('shown.bs.modal', function () {
+            createMap();
+            map.on("load", function () {
+                this.setCenter([lng, lat]);
+                map.setZoom(zoom);
+            });
+
+            let markerElement = document.createElement("i");
+            markerElement.className = "fa fa-3x fa-map-marker text-primary";
+            marker = new mapboxgl.Marker(markerElement)
+            .setLngLat([lng, lat])
+            .addTo(map);
+
+            addClickEvent(map);
+
+        });
+
+    } else {
+        map.setCenter([lng, lat]);
+        map.setZoom(zoom);
+        marker.setLngLat([lng, lat]);
+    }
+}
+
+function createMap() {
+    map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        zoom: 9
+    });
+}
+
+function addClickEvent(map) {
+    map.on("click", function(e) {
+        let lat = e.lngLat.lat;
+        let lng = e.lngLat.lng;
+
+        marker.setLngLat([lng, lat]);
+        swal({
+            title: "¿Guardar posición seleccionada?",
+            text: "Seleccionado: " + lng + ", " + lat,
+            icon: "info",
+            buttons: {
+                confirm: "Guardar",
+                cancel: "Cancelar",
+            }
+        })
+        .then(function (isConfirm) {
+            if (isConfirm) {
+                swal.close();
+                $("#mapModal").modal("hide");
+                savePosition(lat, lng);
+            }
+        });
+    });
+}
+
+function savePosition(lat, lng) {
+    let form = document.getElementById("updateCoordinatesForm");
+    form.action = form.action.replace("/id/", "/" + building_id + "/");
+    form.querySelector("[name=latitude]").value = lat;
+    form.querySelector("[name=longitude]").value = lng;
+    form.submit();
 }
 
 $(function () {
