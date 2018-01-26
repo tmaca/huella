27389f747ca6f585ddc0d8c55cf9b940d5d5f517
alexbcberio <alexbcberio@gmail.com>
2018-01-26 13:06:07 +0100

    Fixed internal error if there were no studies

--- resources/views/user/alcances.blade.php
+++ resources/views/user/alcances.blade.php
@@ -6,46 +6,50 @@
 
     <div class="container">
         <ul class="nav nav-tabs mb-2" id="myTab" role="tablist">
-            @foreach($studies as $study)
-            <li class="nav-item">
-                <a class="nav-link{{ $errors->first("inputYear") == $study->year ? " active" : "" }}" id="study-{{ $study->year }}-tab" data-toggle="tab" href="#study-{{ $study->year }}">
-                    @if($study->carbon_footprint)
-                    <i class="fa fa-paw" title="Huella calculada"></i>
-                    @else
-                    <i class="fa fa-exclamation-triangle text-warning" title="Huella no calculada"></i>
-                    @endif
-                    {{ $study->year }}
-                </a>
-            </li>
-            @endforeach
+            @if(count($studies) > 0)
+                @foreach($studies as $study)
+                <li class="nav-item">
+                    <a class="nav-link{{ $errors->first("inputYear") == $study->year ? " active" : "" }}" id="study-{{ $study->year }}-tab" data-toggle="tab" href="#study-{{ $study->year }}">
+                        @if($study->carbon_footprint)
+                        <i class="fa fa-paw" title="Huella calculada"></i>
+                        @else
+                        <i class="fa fa-exclamation-triangle text-warning" title="Huella no calculada"></i>
+                        @endif
+                        {{ $study->year }}
+                    </a>
+                </li>
+                @endforeach
+            @endif
 
             <li class="nav-item">
-                <a class="nav-link{{ empty($errors->first("inputYear")) == $study->year ? " active" : "" }}" id="contact-tab" data-toggle="tab" href="#contact">
+                <a class="nav-link{{ empty($errors->first("inputYear")) ? " active" : "" }}" id="contact-tab" data-toggle="tab" href="#contact">
                     <i class="fa fa-plus"></i>
                     Crear nuevo
                 </a>
             </li>
         </ul>
         <div class="tab-content" id="myTabContent">
-            @foreach($studies as $study)
-            <div class="tab-pane fade{{ $errors->first("inputYear") == $study->year ? " show active" : "" }}" id="study-{{ $study->year }}">
+            @if(count($studies) > 0)
+                @foreach($studies as $study)
+                <div class="tab-pane fade{{ $errors->first("inputYear") == $study->year ? " show active" : "" }}" id="study-{{ $study->year }}">
 
-                @if(!$study->carbon_footprint)
-                <div class="alert alert-warning">
-                    <i class="fa fa-exclamation-triangle"></i>
-                    <strong>¡Atención!</strong> no se ha realizado aún el calculo de la huella. Finalize la compleción de los datos y pulse sobre el botón "calcular huella de carbono". Una vez calculada la huella no podrá editar los campos.
+                    @if(!$study->carbon_footprint)
+                    <div class="alert alert-warning">
+                        <i class="fa fa-exclamation-triangle"></i>
+                        <strong>¡Atención!</strong> no se ha realizado aún el calculo de la huella. Finalize la compleción de los datos y pulse sobre el botón "calcular huella de carbono". Una vez calculada la huella no podrá editar los campos.
+                    </div>
+                    @else
+                    <div class="alert alert-info">
+                        <strong><i class="fa fa-paw"></i></strong> Valor de la huella, {{ $study->carbon_footprint }}
+                    </div>
+                    @endif
+
+                    @include("forms.user.alcances", ["study" => $study])
                 </div>
-                @else
-                <div class="alert alert-info">
-                    <strong><i class="fa fa-paw"></i></strong> Valor de la huella, {{ $study->carbon_footprint }}
-                </div>
-                @endif
+                @endforeach
+            @endif
 
-                @include("forms.user.alcances", ["study" => $study])
-            </div>
-            @endforeach
-
-            <div class="tab-pane fade{{ empty($errors->first("inputYear")) == $study->year ? " show active" : "" }}" id="contact">
+            <div class="tab-pane fade{{ empty($errors->first("inputYear")) ? " show active" : "" }}" id="contact">
                 @include("forms.user.alcances", ["study" => new App\Models\Study])
             </div>
         </div>
