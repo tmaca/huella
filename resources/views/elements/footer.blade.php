442efb2b71fb6d86fb7c92f8db15ea59a865923b
alexbcberio <alexbcberio@gmail.com>
2018-01-29 09:14:59 +0100

    Added terms of service link on footer

--- resources/views/elements/footer.blade.php
+++ resources/views/elements/footer.blade.php
@@ -4,6 +4,10 @@
 
     <div id="textofooter">
         <p>{{ config("app.name") }} &copy; 2018</p>
+
+        <p class="text-center">
+            <a href="{{ route("termsOfService") }}">Terminos de servicio</a>
+        </p>
     </div>
 
 </footer>
