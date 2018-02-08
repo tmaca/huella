de15d76129511d8ad82bad1de240f189e382c3e8
jbustoro <jbustoro16dw@ikzubirimanteo.com>
2018-02-08 21:00:41 +0100

    added function to generate api token

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,15 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Models\Building');
     }
+
+    /**
+     * Api token
+     */
+    public function generateToken()
+    {
+        $this->api_token = str_random(60);
+        $this->save();
+
+        return $this->api_token;
+    }
 }
