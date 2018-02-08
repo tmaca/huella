d17595d8219e81a4a6ddf9f0ac9776b98ec3938d
jbustoro <jbustoro16dw@ikzubirimanteo.com>
2018-02-08 20:57:58 +0100

    added login function for api

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -96,4 +96,32 @@ class LoginController extends Controller
         }
 
     }
+
+    public function loginApi(Request $request)
+    {
+        $this->validateLogin($request);
+
+        if ($this->attemptLogin($request)) {
+            $user = $this->guard()->user();
+            $user->generateToken();
+
+            return response()->json([
+                'data' => $user->toArray(),
+            ]);
+        }
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    /*public function logout(Request $request)
+    {
+        $user = Auth::guard('api')->user();
+
+        if ($user) {
+            $user->api_token = null;
+            $user->save();
+        }
+
+        return response()->json(['data' => 'User logged out.'], 200);
+    }*/
 }
