5da6cf0a14fdb6ce4f699ce747fee5ec521bb19c
jbustoro <jbustoro16dw@ikzubirimanteo.com>
2018-02-08 21:01:38 +0100

    added api routes for building

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Models\Building;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,17 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post("register", "Auth\RegisterController@registerApi");
+Route::post("login", "Auth\LoginController@loginApi");
+Route::post('logout', 'Auth\LoginController@logout');
+
+// Building routes
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::get("buildings", "ApiController@showAllBuildings");
+    Route::get("buildings/{building}", "ApiController@showBuilding");
+    Route::post('buildings', 'ApiController@storeBuilding');
+    Route::post('buildings/{building}', 'ApiController@updateBuilding');
+    Route::delete('buildings/{building}', 'ApiController@deleteBuilding');
 });
