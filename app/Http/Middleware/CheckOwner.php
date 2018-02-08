13630eadf9f4ba6ed496a8cf868ebc8d364f5a13
jbustoro <jbustoro16dw@ikzubirimanteo.com>
2018-02-08 21:02:55 +0100

    added middleware to check the building owner before update & delete

---
+++ app/Http/Middleware/CheckOwner.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckOwner
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->building->user_id == $request->user()->id) {
+            return $next($request);
+        }
+
+        return response()->json([
+            "errorCode" => 403,
+            "error" => "Not allowed"
+        ], 403);
+    }
+}
